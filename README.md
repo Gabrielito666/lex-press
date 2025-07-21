e199377e9f558d0b61a7db1df65f53eb76c8b408
Gabrielito666 <galileo.fandango@gmail.com>
2025-07-21 15:16:45 -0400

    2.0.3

--- README.md
+++ README.md
@@ -3,24 +3,23 @@
 DOCUMENTATION IN PROGRESS
 
 This is a beta version.
-
 ---
 En modo desarrollo
 
-se buildea todo en .lex y se lanza el server
+se buildea todo en .lex-press y se lanza el server
 
 En modo build
-se buildea todo en .lex
+se buildea todo en .lex-press
 
 En modo start
-se lanza el server usando .lex
+se lanza el server usando .lex-press
 ---
 
 npm run dev
 NODE_ENV=dev node index.js
 
 npm run build
-BUILD_MODE=true ENTRY_POINT=./index.js node index.js
+node node_modules/lex-press/build/index.js [your-entry-point.js]
 
 npm run start
 NODE_ENV=production .lex-press/server.js
--- build/index.js
+++ build/index.js
@@ -2,15 +2,25 @@ const [_, __, serverEntrypoint] = process.argv;
 const child_process = require("child_process");
 const buildServer = require("../lib/build-server");
 
-child_process.exec(`BUILD_MODE="true" NODE_ENV="development" node ${serverEntrypoint}`, { stdio: "inherit" })
+function execPromise(cmd, opts) {
+    return new Promise((resolve, reject) => {
+        const proc = child_process.exec(cmd, opts, (err, stdout, stderr) => {
+            if (err) return reject(err);
+            resolve({ stdout, stderr });
+        });
+        // Si quieres heredar la salida en tiempo real:
+        if (proc.stdout) proc.stdout.pipe(process.stdout);
+        if (proc.stderr) proc.stderr.pipe(process.stderr);
+    });
+}
+
+execPromise(`BUILD_MODE="true" NODE_ENV="development" node ${serverEntrypoint}`)
 .then(() => buildServer(serverEntrypoint))
-.then(() =>
-{
+.then(() => {
     console.log("Server and files built (development)");
     process.exit(0);
 })
-.catch(err =>
-{
+.catch(err => {
     console.error(err);
     process.exit(1);
 });
